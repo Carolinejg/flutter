b065147cdd8e063af504b4a52b98f8718c489f9e
Caroline Souza <loh_jg@hotmail.com>
2021-02-26 13:30:23 -0300

    Criando a interface do botão

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -8,12 +8,33 @@ main(){
 class PerguntaApp extends StatelessWidget{
 
   Widget build(BuildContext context){
+    final List<String> perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual o seu animal favorito?'
+    ];
     return  MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Perguntas'),
         ),
-        body: Text('Olá flutter!!'),
+        body: Column(
+          children: <Widget>[
+            Text(perguntas.elementAt(0)),
+            RaisedButton(
+              child: Text('Resposta 1'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Resposta 2'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Resposta 3'),
+              onPressed: null,
+            )
+            
+          ],
+        ),
       ),
 
     );
