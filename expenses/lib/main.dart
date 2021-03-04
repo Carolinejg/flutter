fb1786164a13787534f15f87c951e08312b4bc1a
Caroline Souza <loh_jg@hotmail.com>
2021-03-04 12:12:55 -0300

    Capturando os dados da caixa de texto

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -15,6 +15,9 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final titleController= TextEditingController();
+  final valueController= TextEditingController();
+
   final _transactions = {
     Transaction(
       id: 't1',
@@ -107,18 +110,24 @@ class MyHomePage extends StatelessWidget {
               child: Column(
                 children:<Widget>[
                   TextField(
+                    controller: titleController,
                     decoration: InputDecoration(
                       labelText: 'Título',
                     ),
                   ),
-                  TextField(decoration: InputDecoration(
+                  TextField(
+                    controller:  valueController,
+                    decoration: InputDecoration(
                       labelText: 'Valor (R\$)',
                     )
                   ),
                   FlatButton(
                     child: Text('Nova transação'), 
                     textColor: Colors.purple,
-                    onPressed: (){},
+                    onPressed: (){
+                      print(titleController.text);
+                      print(valueController.text);
+                    },
                   )
                 ]
               ),
