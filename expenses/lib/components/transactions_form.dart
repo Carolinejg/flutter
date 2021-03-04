96eaf2ab5dcfad361b0c33255a33d2bf0821315e
Caroline Souza <loh_jg@hotmail.com>
2021-03-04 20:14:27 -0300

    adicionando função submite para o teclado

--- expenses/lib/components/transactions_form.dart
+++ expenses/lib/components/transactions_form.dart
@@ -7,6 +7,16 @@ class TransactionForm extends StatelessWidget {
   final void Function(String, double) onSubmit;
   //para que o transactionform converse com o seu pai USER
   TransactionForm(this.onSubmit);
+
+  _submitForm(){
+    final title =titleController.text;
+    final value =double.tryParse(valueController.text) ?? 0.0;
+
+    if(title.isEmpty || value <=0){
+      return;
+    }
+    onSubmit(title,value);
+  }
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -16,23 +26,22 @@ class TransactionForm extends StatelessWidget {
           child: Column(children: <Widget>[
             TextField(
               controller: titleController,
+              onSubmitted: (_)=> _submitForm(),
               decoration: InputDecoration(
                 labelText: 'Título',
               ),
             ),
             TextField(
                 controller: valueController,
+                keyboardType: TextInputType.numberWithOptions(decimal: true),
+                onSubmitted: (_)=> _submitForm(),
                 decoration: InputDecoration(
                   labelText: 'Valor (R\$)',
                 )),
             FlatButton(
               child: Text('Nova transação'),
               textColor: Colors.purple,
-              onPressed: () {
-                final title =titleController.text;
-                final value =double.tryParse(valueController.text) ?? 0.0;
-                onSubmit(title,value);
-              },
+              onPressed: _submitForm,
             )
           ]),
         ));
