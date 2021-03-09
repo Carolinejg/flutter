b695182f92f1a6d4186f862bba92fa48676ef9e9
Caroline Souza <loh_jg@hotmail.com>
2021-03-09 15:03:29 -0300

    criando barra de gastos preenchidos por cada gosto de cada dia

--- expenses/lib/components/chart.dart
+++ expenses/lib/components/chart.dart
@@ -35,9 +35,15 @@ class Chart extends StatelessWidget {
       };
     });
   }
+
+  double get _weekTotalValue{
+    return groupedTransactions.fold(0.0, (sum, tr){
+      return sum + tr['value'];
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    groupedTransactions;
+    
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(3),//não ficar grudandao na margin do card 
@@ -46,7 +52,7 @@ class Chart extends StatelessWidget {
           return ChartBar(
             label: tr['day'],
             value: tr['value'],
-            percentage: 0.5,
+            percentage: (tr['value'] as double )/_weekTotalValue,
           );
         }).toList(),
       ),
