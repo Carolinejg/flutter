4828c725ed5f289a4924aaf649aa6008e0f7049d
Caroline Souza <loh_jg@hotmail.com>
2021-03-11 11:28:35 -0300

    estruturando tela de categorias e renderizando na tela

--- meals/lib/screens/categories_screen.dart
+++ meals/lib/screens/categories_screen.dart
@@ -1,19 +1,28 @@
 import 'package:flutter/material.dart';
-//representa  a tela de categorias 
+import 'package:meals/components/category_item.dart';
+import 'package:meals/data/dummy.dart';
+
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(//os elementos são organizados em forma de grade 
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,//cada elemento com uma extensão de no máximo 200 px
-        childAspectRatio: 3 / 2, 
-        crossAxisSpacing: 20, //espaçamento no eixo cruzado
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Vamos Cozinhar?'),
+      ),
+      body: GridView(//os elementos são organizados em forma de grade 
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,//cada elemento com uma extensão de no máximo 200 px
+          childAspectRatio: 3 / 2, 
+          crossAxisSpacing: 20, //espaçamento no eixo cruzado
+          mainAxisSpacing: 20,
+          
+        ),
+        children: DUMMY_CATEGORIES.map((cat) {
+          return CategoryItem(cat);
+        }).toList(), //percorrer cada categoria e converter cada categoria para o componente criado
+
         
       ),
-      children: <Widget>[
-        
-      ],
     );
   }
 }
