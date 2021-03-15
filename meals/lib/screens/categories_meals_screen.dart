653b13a8ea02a89d7bd30eb26af509437dacd0b8
Caroline Souza <loh_jg@hotmail.com>
2021-03-15 10:14:37 -0300

    selecionando categoria por id

--- meals/lib/screens/categories_meals_screen.dart
+++ meals/lib/screens/categories_meals_screen.dart
@@ -1,20 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/category.dart';
+import '../data/dummy.dart';
 
-///tela contendo todas as comidas de uma categoria 
+///tela contendo todas as comidas de uma categoria
 
 class CategoriesMealsScreen extends StatelessWidget {
-  
-
   @override
   Widget build(BuildContext context) {
     final category = ModalRoute.of(context).settings.arguments as Category;
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(category
+          .id); //se o id da category está dentro está dendtro das categorias da meals retorna true
+    }).toList();
     return Scaffold(
       appBar: AppBar(
-        title: Text(category.title) ,
+        title: Text(category.title),
       ),
-      body: Center(
-        child: Text('Receitas por Categorias ${category.id}'),
+      body: ListView.builder(
+        itemCount: categoryMeals.length,
+        itemBuilder: (ctx, index) {
+          return Text(categoryMeals[index].title);
+        },
       ),
     );
   }
